f29dd00716d9820dea381fdbec6bb822b4fb3faf
synchronized <synchronized@yeah.net>
2026-08-13 11:47:46 +0800

    fix: 修正 Linux 便携目标平台定义

--- src/compat/skynet/start/skyuv_daemon.c
+++ src/compat/skynet/start/skyuv_daemon.c
@@ -23,6 +23,8 @@ void *skynet_lalloc(void *ptr, size_t old_size, size_t new_size) {
 	return realloc(ptr, new_size);
 }
 
+#ifdef _WIN32
 struct tm *localtime_r(const time_t *time_value, struct tm *result) {
 	return localtime_s(result, time_value) == 0 ? result : NULL;
 }
+#endif
