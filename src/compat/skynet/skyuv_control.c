34047062da0bd7ad4252b20e9028206624a9798d
synchronized <synchronized@yeah.net>
2026-08-14 09:56:47 +0800

    fix: 收窄日志控制内部 ABI

--- src/compat/skynet/skyuv_control.c
+++ src/compat/skynet/skyuv_control.c
@@ -1,11 +1,21 @@
 #include "skyuv_control.h"
 
-#include "skynet_handle.h"
-#include "skynet_mq.h"
-#include "skynet_server.h"
+#include <stddef.h>
+#include <stdint.h>
+
+struct skynet_message {
+	uint32_t source;
+	int session;
+	void *data;
+	size_t sz;
+};
+
+uint32_t skynet_handle_findname(const char *name);
+int skynet_context_push(uint32_t handle, struct skynet_message *message);
 
 enum {
 	SKYUV_SKYNET_PTYPE_SYSTEM = 4,
+	SKYUV_SKYNET_MESSAGE_TYPE_SHIFT = (sizeof(size_t) - 1) * 8,
 };
 
 int skyuv_skynet_reopen_log(void) {
@@ -18,6 +28,6 @@ int skyuv_skynet_reopen_log(void) {
 	message.source = 0;
 	message.session = 0;
 	message.data = NULL;
-	message.sz = (size_t)SKYUV_SKYNET_PTYPE_SYSTEM << MESSAGE_TYPE_SHIFT;
+	message.sz = (size_t)SKYUV_SKYNET_PTYPE_SYSTEM << SKYUV_SKYNET_MESSAGE_TYPE_SHIFT;
 	return skynet_context_push(logger, &message) == 0;
 }
