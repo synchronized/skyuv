300e311ba6a891b3ee6081330aeb6846584fbf76
synchronized <synchronized@yeah.net>
2026-08-17 09:49:38 +0800

    test: 验证跨模块内存所有权

--- tests/dynamic_module.c
+++ tests/dynamic_module.c
@@ -1,1 +1,23 @@
+#include <stddef.h>
+#include <string.h>
+
+#include <skyuv/memory.h>
+
 int skyuv_test_module_value = 42;
+
+void *skyuv_test_module_allocate(size_t size, int value) {
+	void *pointer = skyuv_malloc(size);
+
+	if (pointer != NULL) {
+		memset(pointer, value, size);
+	}
+	return pointer;
+}
+
+void *skyuv_test_module_reallocate(void *pointer, size_t size) {
+	return skyuv_realloc(pointer, size);
+}
+
+void skyuv_test_module_release(void *pointer) {
+	skyuv_free(pointer);
+}
--- tests/test_dynamic.c
+++ tests/test_dynamic.c
@@ -1,18 +1,35 @@
 #include <setjmp.h>
 #include <stdarg.h>
 #include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include <cmocka.h>
 
 #include <skyuv/dynamic.h>
+#include <skyuv/memory.h>
+
+typedef void *(*skyuv_test_allocate_fn)(size_t size, int value);
+typedef void *(*skyuv_test_reallocate_fn)(void *pointer, size_t size);
+typedef void (*skyuv_test_release_fn)(void *pointer);
+
+static const char *module_path;
+
+static void load_function(skyuv_dl *library, const char *name, void *function, size_t size) {
+	void *symbol = NULL;
+
+	assert_int_equal(size, sizeof(symbol));
+	assert_int_equal(skyuv_dlsym(library, name, &symbol), SKYUV_OK);
+	memcpy(function, &symbol, size);
+}
 
 static void test_invalid_arguments(void **state) {
 	skyuv_dl library = SKYUV_DL_INITIALIZER;
 	void *symbol;
 
 	(void)state;
-	assert_int_equal(skyuv_dlopen(NULL, SKYUV_TEST_MODULE_PATH), SKYUV_ERROR_INVALID_ARGUMENT);
+	assert_int_equal(skyuv_dlopen(NULL, module_path), SKYUV_ERROR_INVALID_ARGUMENT);
 	assert_int_equal(skyuv_dlopen(&library, NULL), SKYUV_ERROR_INVALID_ARGUMENT);
 	assert_int_equal(skyuv_dlsym(NULL, "symbol", &symbol), SKYUV_ERROR_INVALID_ARGUMENT);
 	assert_int_equal(skyuv_dlsym(&library, NULL, &symbol), SKYUV_ERROR_INVALID_ARGUMENT);
@@ -34,7 +51,7 @@ static void test_open_failure(void **state) {
 	error = skyuv_dlerror(&library);
 	assert_non_null(error);
 	assert_true(strlen(error) > 0);
-	assert_int_equal(skyuv_dlopen(&library, SKYUV_TEST_MODULE_PATH), SKYUV_ERROR_INVALID_STATE);
+	assert_int_equal(skyuv_dlopen(&library, module_path), SKYUV_ERROR_INVALID_STATE);
 	skyuv_dlclose(&library);
 	assert_null(skyuv_dlerror(&library));
 }
@@ -44,7 +61,7 @@ static void test_load_and_find_symbol(void **state) {
 	void *symbol;
 
 	(void)state;
-	assert_int_equal(skyuv_dlopen(&library, SKYUV_TEST_MODULE_PATH), SKYUV_OK);
+	assert_int_equal(skyuv_dlopen(&library, module_path), SKYUV_OK);
 	assert_int_equal(skyuv_dlsym(&library, "skyuv_test_module_value", &symbol), SKYUV_OK);
 	assert_non_null(symbol);
 	assert_int_equal(*(const int *)symbol, 42);
@@ -55,12 +72,61 @@ static void test_load_and_find_symbol(void **state) {
 	skyuv_dlclose(&library);
 }
 
-int main(void) {
+static void test_cross_module_memory_ownership(void **state) {
+	unsigned char *module_memory;
+	unsigned char *core_memory;
+	size_t cycle;
+	size_t index;
+
+	(void)state;
+	for (cycle = 0; cycle < 64; ++cycle) {
+		skyuv_dl library = SKYUV_DL_INITIALIZER;
+		skyuv_test_allocate_fn module_allocate = NULL;
+		skyuv_test_reallocate_fn module_reallocate = NULL;
+		skyuv_test_release_fn module_release = NULL;
+
+		assert_int_equal(skyuv_dlopen(&library, module_path), SKYUV_OK);
+		load_function(&library, "skyuv_test_module_allocate", &module_allocate,
+					  sizeof(module_allocate));
+		load_function(&library, "skyuv_test_module_reallocate", &module_reallocate,
+					  sizeof(module_reallocate));
+		load_function(&library, "skyuv_test_module_release", &module_release,
+					  sizeof(module_release));
+
+		module_memory = module_allocate(128, 0x5a);
+		assert_non_null(module_memory);
+		for (index = 0; index < 128; ++index) {
+			assert_int_equal(module_memory[index], 0x5a);
+		}
+
+		core_memory = skyuv_malloc(64);
+		assert_non_null(core_memory);
+		memset(core_memory, 0xa5, 64);
+		core_memory = module_reallocate(core_memory, 256);
+		assert_non_null(core_memory);
+		for (index = 0; index < 64; ++index) {
+			assert_int_equal(core_memory[index], 0xa5);
+		}
+		module_release(core_memory);
+
+		skyuv_dlclose(&library);
+		/* 分配发生在模块调用期间，但所有权属于主程序，卸载后仍可由核心释放。 */
+		skyuv_free(module_memory);
+	}
+}
+
+int main(int argc, char **argv) {
 	const struct CMUnitTest tests[] = {
 		cmocka_unit_test(test_invalid_arguments),
 		cmocka_unit_test(test_open_failure),
 		cmocka_unit_test(test_load_and_find_symbol),
+		cmocka_unit_test(test_cross_module_memory_ownership),
 	};
+
+	if (argc != 2) {
+		return EXIT_FAILURE;
+	}
+	module_path = argv[1];
 
 	return cmocka_run_group_tests(tests, NULL, NULL);
 }
